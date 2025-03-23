06d03167945dc8ef76169e47925f29dc2a8bd85d
Muniraj97 <muniram14meiet92@gmail.com>
2025-03-23 15:38:42 +0530

    hiiii

---
+++ src/org/test/A.java
@@ -0,0 +1,9 @@
+package org.test;
+
+public class A extends B {
+public A() {
+	super(23);
+	System.out.println("name is muni");
+}
+A w=new A();
+}
---
+++ src/org/test/B.java
@@ -0,0 +1,9 @@
+package org.test;
+
+public class B {
+	public B(int age) {
+		System.out.println("age is"+age);
+	}
+	
+
+}

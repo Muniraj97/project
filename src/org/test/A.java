55d0d5ccf343ed26c7465c95ebeb87075e6004bf
Muniraj97 <muniram14meiet92@gmail.com>
2025-03-23 16:03:14 +0530

    hlo

--- src/org/test/A.java
+++ src/org/test/A.java
@@ -4,6 +4,8 @@ public class A extends B {
 public A() {
 	super(23);
 	System.out.println("name is muni");
+	System.out.println("name is raj");
+	System.out.println("name is ram");
 }
 A w=new A();
 }
